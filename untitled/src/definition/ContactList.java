2c91225705429689c754f9d70795128ef2c37e65
Chaitanya2801 <chaitanya.agarwal_iot18@gla.ac.in>
2020-02-24 21:58:53 +0530

    creating the fields in the contact class

--- untitled/src/definition/ContactList.java
+++ untitled/src/definition/ContactList.java
@@ -2,7 +2,15 @@ package definition;
 
 import adt.ContactAdt;
 
+import java.util.ArrayList;
+
 public class ContactList<Person> implements ContactAdt {
+    ArrayList<String> arrayOfNames = new ArrayList<>();
+    ArrayList<String> arrayOfFirstName = new ArrayList<>();
+    private Node<Person> head;
+    private int size = 0;
+    private int counter;
+
     @Override
     public boolean add(Object o) {
         return false;
