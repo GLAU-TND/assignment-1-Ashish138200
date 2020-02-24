845807362259f37d92929ed7da10c06e49304f0d
Chaitanya2801 <chaitanya.agarwal_iot18@gla.ac.in>
2020-02-24 21:58:07 +0530

    implementing the contactadt interface and implementing its methods

--- untitled/src/definition/ContactList.java
+++ untitled/src/definition/ContactList.java
@@ -1,4 +1,25 @@
 package definition;
 
-public class ContactList {
+import adt.ContactAdt;
+
+public class ContactList<Person> implements ContactAdt {
+    @Override
+    public boolean add(Object o) {
+        return false;
+    }
+
+    @Override
+    public boolean remove(int index) {
+        return false;
+    }
+
+    @Override
+    public void search(String name) {
+
+    }
+
+    @Override
+    public void viewAll() {
+
+    }
 }
