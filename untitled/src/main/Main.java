e98e4969a9b6aa7240ff64d6636b40b9efcdea1a
Chaitanya2801 <chaitanya.agarwal_iot18@gla.ac.in>
2020-02-24 22:21:19 +0530

    creating case 1

--- untitled/src/main/Main.java
+++ untitled/src/main/Main.java
@@ -1,6 +1,7 @@
 package main;
 
 import definition.ContactList;
+import definition.Person;
 
 import java.util.Scanner;
 
@@ -9,5 +10,46 @@ public class Main {
     ContactList contactList = new ContactList();
     String FirstName;
     String LastName;
-}
+    String[] contactNumber = new String[3];
+    String email;
+    boolean flag;
+    while (flag = true) {
+        System.out.println("Welcome to Chaitanya contact list");
+        System.out.println("Press 1 to add a new contact\n " +
+                "Press 2 to view all contacts\n" +
+                "Press 3 to search for a contact\n" +
+                "Press 4 to delete a contact\n" +
+                "Press 5 to exit program");
+        int number = scanner.nextInt();
+        switch (number){
+            case 1:
+                System.out.println("You have chosen to add a new contact: \n" +
+                        "Please enter the name of the person");
+                System.out.println("First name");
+                FirstName = scanner.nextLine();
+                System.out.println("Last Name");
+                LastName = scanner.nextLine();
+                contactList.setName(FirstName, LastName);
+                contactList.setFirstNameInList(FirstName);
+                System.out.println("Contact Number:");
+                contactNumber[0] = scanner.nextLine();
+                for (int i = 1; i <= 2; i++) {
+                    System.out.println("Would you like to add new Number?");
+                    char response = scanner.next().charAt(0);
+                    if (response == 'y') {
+                        contactNumber[i] = scanner.next();
+                    } else {
+                        break;
+                    }
+                }
+                System.out.println("Enter the Email:");
+                email = scanner.next();
+                Person person = new Person(FirstName, LastName, contactNumber, email);
+                contactList.add(person);
+                break;
+            case 2:
+                contactList.viewAll();
+                break;
+        }
+    }
 
