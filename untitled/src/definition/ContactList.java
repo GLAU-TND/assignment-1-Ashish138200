aa3e132087188edfd86502c1159073692a2c23d5
Chaitanya2801 <chaitanya.agarwal_iot18@gla.ac.in>
2020-02-24 22:10:43 +0530

    creating the private node class and creating the constructor and getter setter of the methods

--- untitled/src/definition/ContactList.java
+++ untitled/src/definition/ContactList.java
@@ -9,6 +9,7 @@ public class ContactList<Person> implements ContactAdt {
     ArrayList<String> arrayOfFirstName = new ArrayList<>();
     private Node<Person> head;
     private int size = 0;
+    private int counter;
 
     public int getSize() {
         return size;
@@ -42,27 +43,135 @@ public class ContactList<Person> implements ContactAdt {
         return response;
     }
 
+    public void setName(String FirstName, String LastName) {
+        arrayOfNames.add(FirstName + " " + LastName);
+    }
 
+    public ArrayList getArrayOfNames() {
+        return arrayOfNames;
+    }
 
-    private int counter;
 
     @Override
-    public boolean add(Object o) {
-        return false;
+    public boolean add(Person personData) {
+        add(size, personData);
+        return true;
+    }
+
+    private Person removeFirst() {
+        Person response = null;
+        Node<Person> temp = head;
+        if (head != null) {
+            head = head.getNext();
+        }
+        if (temp != null) {
+            size--;
+            response = temp.getData();
+        }
+        return response;
+    }
+
+    private Person removeAfter(Node<Person> node) {
+        Person response = null;
+        Node<Person> temp = node.getNext();
+        if (temp != null) {
+            node.next = temp.getNext();
+            size--;
+            response = temp.getData();
+        }
+        if (temp == null) {
+            node = null;
+            size--;
+        }
+        return response;
     }
 
     @Override
     public boolean remove(int index) {
-        return false;
+        boolean response = false;
+        if (index - 1 < 0 || (index - 1) > getSize()) {
+            throw new IndexOutOfBoundsException(Integer.toString(index - 1));
+        } else if (index - 1 == 0) {
+            removeFirst();
+            arrayOfNames.remove(index - 1);
+            response = true;
+        } else {
+            Node<Person> previousNode = getNode(index - 1);
+            removeAfter(previousNode);
+            arrayOfNames.remove(index - 1);
+            response = true;
+
+        }
+        return response;
+    }
+
+    public void setFirstNameInList(String name) {
+        arrayOfFirstName.add(name);
+    }
+
+    public ArrayList getArrayOfFirstName() {
+        return arrayOfFirstName;
     }
 
     @Override
     public void search(String name) {
-
+        for (int i = 0; i < arrayOfFirstName.size(); i++) {
+            if (name.compareTo(arrayOfFirstName.get(i)) == 0) {
+                counter++;
+            }
+        }
+        if (counter != 0) {
+            System.out.println(counter + "Match Found");
+            for (int i = 0; i < arrayOfFirstName.size(); i++) {
+                if (name.compareTo(arrayOfFirstName.get(i)) == 0) {
+                    Node<Person> personNode = getNode(i);
+                    System.out.println(personNode.getData().toString());
+                }
+            }
+        } else {
+            System.out.println("No Match Found");
+        }
     }
 
     @Override
     public void viewAll() {
+        if (size != 0) {
+            System.out.println("--Here are all your contacts--");
+            for (int i = 0; i < size; i++) {
+                Person data = this.getNode(i).getData();
+                System.out.println(data);
+            }
+        } else {
+            System.out.println("No Results Found!");
+        }
+    }
+
+    private static class Node<Person> {
+        private Person data;
+        private Node<Person> next;
+
+        public Person getData() {
+            return data;
+        }
+
+        public void setData(Person data) {
+            this.data = data;
+        }
+
+        public Node<Person> getNext() {
+            return next;
+        }
+
+        public void setNext(Node<Person> next) {
+            this.next = next;
+        }
+
+        public Node(Person data, Node<Person> next) {
+            this.data = data;
+            this.next = next;
+        }
+
+
 
     }
 }
